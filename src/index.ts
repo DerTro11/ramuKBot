bfa238fb95cddf74581aa2845ad26743f5dbb1f1
DerTro11 <Maximilian.C.Foerster@gmx.de>
2025-02-26 23:16:04 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,10 @@ clnt.on("ready", function(){
     console.log(`Bot ${clnt.user?.tag} is ready`)
 })
 
-let eventFiles = readdirSync("./src/events");
-
+const eventFiles = readdirSync("./src/events");
+for (let index = 0; index < eventFiles.length; index++) {
+  
+}
 
 
 clnt.login(process.env.DCToken);
