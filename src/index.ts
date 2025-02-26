59401d6f19635da30d78c4093ef1d161c5fdd233
DerTro11 <Maximilian.C.Foerster@gmx.de>
2025-02-26 12:40:08 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import {Client, IntentsBitField, Partials} from "discord.js";
 require('dotenv').config();
 
 const clnt = new Client({
-    intents: Object.values(IntentsBitField.Flags),
-    partials: Object.values(Partials),
+    intents: Object.values(IntentsBitField.Flags) as [],
+    partials: Object.values(Partials) as []
+  });
+
+clnt.on("ready", function(){
+    console.log(`Bot ${clnt.user?.tag} is ready`)
 })
-
-
 
 clnt.login(process.env.DCToken);
