44c4c03e85eb190cc2b1158967dab13ec39e0287
DerTro11 <Maximilian.C.Foerster@gmx.de>
2025-03-04 11:19:47 +0100

    Update guildMemberAdd.ts

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -6,8 +6,9 @@ const Handler : EventHandler<"guildMemberAdd"> = {
     on(member) {
         try{
             member.setNickname(convertLatinToCyrillic( member.nickname || member.displayName )); // because yes
+            member.roles.add("1282955086334787678");
         }catch(er){
-            console.error(`Failed to update nickname of member ${member.displayName}, please make sure the bot has Nickname permissions.` )
+            console.error(`Failed to update nickname of member ${member.displayName}, please make sure the bot has sufficent permissions.` );
         }
     },
 } 
