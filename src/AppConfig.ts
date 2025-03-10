dd5d9122acd185e8acfecd4060fd5e336a97d443
DerTro11 <Maximilian.C.Foerster@gmx.de>
2025-03-10 15:50:03 +0100

    Update AppConfig.ts

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,6 +1,6 @@
 export default {
     "UpdateSlashCMDsOnRun": true,
-    "MainGuild": "1250311077678288967",
-    "GameNightVCId": "1250311077678288971",
-    "GameNightAnnoucmentChnlId": "1347622474887729274" 
+    "MainGuild": "1282954614072676383",
+    "GameNightVCId": "1282954614072676387",
+    "GameNightAnnoucmentChnlId": "1282954614072676386" 
 }
