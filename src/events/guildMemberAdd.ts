10d46f222329b7f6ec385ffd500f4a835a42574c
DerTro11 <Maximilian.C.Foerster@gmx.de>
2025-03-28 19:08:57 +0100

    Update guildMemberAdd.ts

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -3,10 +3,10 @@ import {convertLatinToCyrillic} from "../Utils/miscUtil";
 
 
 const Handler : EventHandler<"guildMemberAdd"> = {
-    on(member) {
+    async on(member) {
         try{
-            member.setNickname(convertLatinToCyrillic( member.nickname || member.displayName )); // because yes
-            member.roles.add("1282955086334787678");
+            await member.setNickname(convertLatinToCyrillic( member.nickname || member.displayName )); // because yes
+            await member.roles.add("1282955086334787678");
         }catch(er){
             console.error(`Failed to update nickname of member ${member.displayName}, please make sure the bot has sufficent permissions.` );
         }
