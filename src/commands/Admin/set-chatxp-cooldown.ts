e85329b082546528948d6e75fd3bd38422ff7a0a
DerTro11 <Maximilian.C.Foerster@gmx.de>
2025-07-13 13:33:19 +0200

    Create set-chatxp-cooldown.ts

---
+++ src/commands/Admin/set-chatxp-cooldown.ts
@@ -0,0 +1,38 @@
+import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import GuildConfig from "../../MongoDB/models/GuildConfig";
+import { Command } from "types";
+
+const CommandBody = new SlashCommandBuilder()
+.setName("set-chatxp-cooldown")
+.setDescription("Sets the cooldown of the Chat XP timer.")
+.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+.setContexts([0])
+.addIntegerOption(option => 
+    option.setName("lenght")
+    .setDescription("The lenght of the cooldown in ms.")
+    .setRequired(true)
+    .setMinValue(1)
+    .setMaxValue(300)
+) as SlashCommandBuilder;
+
+
+export const Cmd : Command = {
+    CommandBody: CommandBody,
+    async execute(Interaction) {
+        await Interaction.deferReply();
+
+        const timeOption = Interaction.options.getInteger("lenght", true);
+        
+        const update =  timeOption !== 60 ? {$set: { ChatXPCooldownMs: timeOption*1000} } : { $unset: {ChatXPCooldownMs: ""} };
+        
+        const config = await GuildConfig.findOneAndUpdate(
+            { GuildID: Interaction.guild?.id },
+            update,
+            { upsert: true, new: true, setDefaultsOnInsert: true  }
+        );
+
+        await Interaction.editReply({content: `✅ Cooldown set to ${timeOption} seconds.`})
+    },
+}
+
+export default Command
