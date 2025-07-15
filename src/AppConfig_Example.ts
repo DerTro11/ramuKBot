d01f2d3825edf5a551f0883fc5e27c2408b7a535
DerTro11 <Maximilian.C.Foerster@gmx.de>
2025-07-15 12:07:51 +0200

    Update AppConfig_Example.ts

--- src/AppConfig_Example.ts
+++ src/AppConfig_Example.ts
@@ -1,5 +1,14 @@
 export default { 
     "UpdateSlashCMDsOnRun": false,
-    "HQGuild": "0"
-    
+    "HQGuild": "0",
+    "baseXPAmounts": {
+        "EventXPPerMinute": 5,
+        "ReactionXPAmount": 10,
+        "PenaltyXPAmount": 30,
+        "EventBonusMultiplier": 1.5,
+        "ChatXPCooldownMs": 60_000,
+        "ChatXPAmount": 1,
+        "BonusXPThreshold_Above": .5,
+        "PenaltyXPThreshold_Below": .25
+    }
 }
